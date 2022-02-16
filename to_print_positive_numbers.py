aa9d7fef722f21245be02068ad66b8640def900b
Asrar-Ahammad <90050859+Asrar-Ahammad@users.noreply.github.com>
2022-02-16 13:13:15 +0530

    To print positive numbers
    This is a program to filter out positive numbers from a list.

---
+++ to_print_positive_numbers.py
@@ -0,0 +1,12 @@
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14, -95, 3]
+print("List1 :",list1)
+print("Output :")
+for x in list1 :
+    if x>0 :
+        print(x)
+print("List2 :",list2)
+print("Output :")
+for x in list2 :
+    if x>0 :
+        print(x)
