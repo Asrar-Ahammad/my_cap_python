294503998cd2c6f4474e4d742ab94c27eaffe394
Asrar-Ahammad <90050859+Asrar-Ahammad@users.noreply.github.com>
2022-02-22 16:42:27 +0530

    Update most_frequent_letters.py

--- most_frequent_letters.py
+++ most_frequent_letters.py
@@ -1,11 +1,12 @@
-def frequency():
-    str = input('Enter a string :')
+def most_frequent():
+    str = input('Please enter a string :')
     di = dict()
     for x in str:
         if x not in di :
             di[x] = 1 
         else :
             di[x]+=1
-    print(di)
+    for x in di:
+        print(x,'=',di[x])
 
-frequency()
+most_frequent()
